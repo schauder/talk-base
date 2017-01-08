8af208dabeece7c2ab6ac8f696662560a7f0a4c7
Jens Schauder <jens.schauder@freenet.de>
2017-01-08 14:13:18 +0100

    moved plugin to separate file

---
+++ buildSrc/src/main/groovy/AsciiReveal.groovy
@@ -0,0 +1,26 @@
+package de.schauderhaft.ascii.reveal
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class AsciiReveal implements Plugin<Project> {
+    void apply(Project project) {
+
+        project.extensions.create("asciiReveal", AsciiRevealExtension)
+
+        project.task('ascii-reveal') {
+            doLast {
+                println project.asciiReveal.srcDir
+                println project.asciiReveal.srcFile
+                println project.asciiReveal.template
+            }
+        }
+    }
+}
+
+class AsciiRevealExtension {
+    def String srcDir = "src/docs/asciidoc"
+    def String srcFile = "index.asciidoc"
+    def template = "black"
+}
+
